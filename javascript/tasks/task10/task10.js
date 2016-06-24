5d02ef5766e3b3e96a70dc1e4bf9f61170e17d29
nadezhdadunkina <ndunkina@pfsweb.com>
2016-06-24 17:14:07 +0300

    Task 10

--- javascript/tasks/task10/task10.js
+++ javascript/tasks/task10/task10.js
@@ -1,7 +1,6 @@
-
-
 // 1.  Format with placeholders
 
+console.log("--------------Problem 1--------------");
 
 String.prototype.format = function(options) {
     var i,
@@ -11,7 +10,6 @@ String.prototype.format = function(options) {
         str = str.replace(regex, options[i]);
     }
     return str;
-
 }
 
 var str1 = "Hello, there! Are you #{name}?";
@@ -20,3 +18,46 @@ var str2 = "My name is #{name} and I am #{age}-years-old";
 console.log(str1.format({ name: 'John' }));
 console.log(str2.format({ name: 'John', age: 13 }));
 
+console.log("-------------------------------------");
+
+
+
+// 2. HTML binding
+
+console.log("--------------Problem 2--------------");
+
+String.prototype.bind = function(str, options) {
+
+    var regex = new RegExp('data-bind-(.+?)="(.+?)"', 'g'),
+        str = this,
+        cont = '';
+
+    str = str.replace(regex, function(all, type, value) {
+        if (type == 'href') {
+            return 'href="' + options[value] + '"';
+        } else if (type == 'content') {
+            cont = options[value];
+            return '';
+        } else if (type == 'div') {
+            cont = options[value];
+            return '';
+        } else {
+            return type + '="' + options[value] + '"';
+        }
+    });
+    if (cont != '') {
+        str = str.replace('</', cont + '</');
+    }
+    return str;
+}
+
+var str = '<a data-bind-content="name" data-bind-href="link" data-bind-class="name" data-bind-id="name" data-bind-custom="custom"></а>';
+console.log(str.bind(str, { name: 'Elena', link: 'http://telerikacademy.com', custom: 'custom attribute' }));
+
+
+
+var str = '<a data-bind-content="name" data-bind-href="link" data-bind-class="name" data-bind-div="age"></а>'
+console.log(str.bind(str, { name: 'Elena', link: 'http://www.w3schools.com', age: '21 years old' }));
+
+
+console.log("-------------------------------------");
